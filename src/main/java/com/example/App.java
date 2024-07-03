6e5bd253e3fa9e3daca050e1fad85448bdbc0d0f
kierraceneee <kierrabroadnax20@gmail.com>
2024-07-03 14:46:06 -0500

    Maven Reporting assignment

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven Reporting!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;  // Returns the sum of the two parameters
+    }
+
+}
